3e1b5cbd41b157ea4ef2c9e31945c2104dfb6834
6soyoung3 <p.soyoung63@gmail.com>
2024-08-09 19:43:41 +0100

    add setting window js functions

---
+++ static/script.js
@@ -0,0 +1,6 @@
+function openSetting(){
+    document.getElementById('setting-window').style.display = "block";
+}
+function closeSetting(){
+    document.getElementById('setting-window').style.display = "none";
+}
